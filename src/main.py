3cbe13f90ad9ccce561c8dae0f3264a416a52cf1
leozamboni <leonardonunes169@gmail.com>
2023-07-23 08:19:01 -0700

    FIX: make get_values endpoint generic

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 # python3 -m flask --app main run --host=0.0.0.0
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
-from settings import webTelemetryDisplaySettings
 from supported.assetto_corsa.assetto_corsa import AssettoCorsa
 import logging
 import socket
@@ -11,6 +10,8 @@ import os
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
+
+from settings import webTelemetryDisplaySettings
 
 gameDir = webTelemetryDisplaySettings['game'].lower().replace(" ", "_")
 displayDir = webTelemetryDisplaySettings['display']
@@ -34,10 +35,14 @@ print(f'Access http://{localNetAddr}:5000/ecu on your mobile')
 @app.route("/")
 @cross_origin()
 def get_values():
-    return {
-		"physics":assettoCorsa.read_physics(),
-		"static": assettoCorsa.read_static(),
-	}
+	if gameDir == 'assetto_corsa':
+		return {
+			"physics":assettoCorsa.read_physics(),
+			"static": assettoCorsa.read_static(),
+		}
+	else:
+		raise ValueError('Game not supported')
+	
 
 @app.route("/ecu")
 @cross_origin()
